86b6080d290f4221854151403927fbe5f42ee229
Devean Cordes <deveandcordes@gmail.com>
2025-08-26 16:19:09 -0400

    added new test

--- homework1_cordes_devean/test_contact_manager.py
+++ homework1_cordes_devean/test_contact_manager.py
@@ -2,7 +2,7 @@ import contact_manager as ctdb
 import time
 
 contact_db = {}
-contact = {
+contact1 = {
     'first_name': 'John',
     'last_name': 'Doe',
     'phone': '555-123-4567',
@@ -18,11 +18,45 @@ contact = {
     'created_date': time.strftime('%Y-%m-%d'),
     'last_modified': time.strftime('%Y-%m-%d')
 }
+contact2 = {
+    'first_name': 'Jane',
+    'last_name': 'Doe',
+    'phone': '555-7654-321',
+    'email': 'jane.doe@email.com',
+    'address': {
+        'street': '321 Side St',
+        'city': 'Anytown',
+        'state': 'ST',
+        'zip_code': '54321'
+    },
+    'category': 'work', # 'personal', 'work', 'family'
+    'notes': 'Met at conference',
+    'created_date': time.strftime('%Y-%m-%d'),
+    'last_modified': time.strftime('%Y-%m-%d')
+}
 
 # DISPLAY TEST
-id = ctdb.add_contact(contact_db, contact)
-print(id)
+id1 = ctdb.add_contact(contact_db, contact1)
+id2 = ctdb.add_contact(contact_db, contact2)
+print(id1)
 print(contact_db)
-ctdb.display_contact(contact_db, id)
+ctdb.display_contact(contact_db, id1)
+print()
 ctdb.list_all_contacts(contact_db)
 # END DISPLAY TEST
+print('\n\n\n')
+# UPDATE+DELETE TEST
+partial = {
+    'email': 'john.doe@gmail.com',
+    'address': {
+        'street': '405 Pine Ln',
+        'city': 'Coolvile'
+    },
+    'notes': 'Epic'
+}
+ctdb.update_contact(contact_db, id1, partial)
+ctdb.display_contact(contact_db, id1)
+print()
+ctdb.delete_contact(contact_db, id1)
+ctdb.list_all_contacts(contact_db)
+# END UPDATE+DELETE TEST
